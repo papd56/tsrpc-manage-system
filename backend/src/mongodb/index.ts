914193a8c1ee57ccaf1349fe974dac28093fe4ba
hawkins <bashen976@gmail.com>
2024-08-14 16:21:06 +0800

    fix

--- backend/src/mongodb/index.ts
+++ backend/src/mongodb/index.ts
@@ -4,7 +4,7 @@ export class Global {
     static db: Db;
 
     static async initDb() {
-        const uri = 'mongodb://127.0.0.1:27017/mongodb?authSource=admin';
+        const uri = 'mongodb://38.12.36.5:27017/mongodb?authSource=admin';
         const client = await new MongoClient(uri).connect();
         this.db = client.db();
     }
